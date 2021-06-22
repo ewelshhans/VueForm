cab4e7085f8a9d7e0bcdb85d9c551e4bdb4665c6
ewelshhans <welshhanse@gmail.com>
2021-06-22 17:49:30 -0400

    deploy

--- form-app/vue.config.js
+++ form-app/vue.config.js
@@ -2,5 +2,7 @@ module.exports = {
   transpileDependencies: [
     'vuetify'
   ],
-  publicPath: '/VueFormApp/'
+  publicPath: process.env.NODE_ENV === 'production'
+    ? '/VueForm/'
+    : '/'
 }
